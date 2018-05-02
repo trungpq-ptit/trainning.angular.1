f207995478af1da6bcb019ab842bdeab9bca56d4
trungpq <trungpq.ptit@gmail.com>
2018-05-02 23:23:16 +0700

    Domain_entities: Duration 18m45s

---
+++ TeduShop.Model/Models/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        [Required]
+        public int GroupId { get; set; }
+
+        public string Target { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+
+        [ForeignKey("GroupId")]
+        public virtual MenuGroup MenuGroup { get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+
+    }
+}
